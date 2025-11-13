44bc6fb346182794b56436a39029bf481114ed95
rafiirajendra <rafi53417@gmail.com>
2025-11-13 15:50:56 +0700

    W12: Jawaban Soal 2

--- 12/stream_rafi/lib/stream.dart
+++ 12/stream_rafi/lib/stream.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ColorStream {
-  
+  final List<Color> colors = [
+    Colors.pink,
+    Colors.deepOrange,
+    Colors.yellowAccent,
+    Colors.lightGreenAccent,
+    Colors.lightBlueAccent,
+    Colors.deepPurple,
+  ];
 }
